9194d90b5a4bc599d20190012a4a0ed095e1bd64
Tiddo Langerak <tiddolangerak@gmail.com>
2016-03-21 10:47:08 +0100

    Added modules-commonjs transformer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
 		require("babel-plugin-transform-es2015-unicode-regex"),
 		require("babel-plugin-transform-es2015-parameters"),
 		require("babel-plugin-transform-es2015-destructuring"),
+		require("babel-plugin-transform-es2015-modules-commonjs"),
 		[require("babel-plugin-transform-regenerator"), { generators: false, asyncGenerators: false }],
 		require("babel-plugin-transform-exponentiation-operator")
 	]
