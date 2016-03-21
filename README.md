9ff544f0cdb7cb79d31525480991c67936ed7871
Tiddo Langerak <tiddolangerak@gmail.com>
2016-03-21 10:42:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Presets for compilation of JavaScript code with all stage-3 features enabled to evergreen browsers.
+
+This preset only compiles those features from stage 3 and up that are not natively supported by any of the following browsers:
+
+- MS Edge 13
+- Mozilla Firefox 45
+- Chrome 49
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins : [
+		require("babel-plugin-transform-es2015-block-scoped-functions"),
+		require("babel-plugin-transform-es2015-unicode-regex"),
+		require("babel-plugin-transform-es2015-parameters"),
+		require("babel-plugin-transform-es2015-destructuring"),
+		[require("babel-plugin-transform-regenerator"), { generators: false, asyncGenerators: false }],
+		require("babel-plugin-transform-exponentiation-operator")
+	]
+};
